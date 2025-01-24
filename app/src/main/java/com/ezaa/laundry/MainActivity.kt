b5a35f9514226c59c37e4fa280087e67fd2484d1
Thoriq Hanif <thoriqmuh123.alkapro@gmail.com>
2025-01-24 08:22:13 +0700

    EzaaLaundry

--- app/src/main/java/com/ezaa/laundry/MainActivity.kt
+++ app/src/main/java/com/ezaa/laundry/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.*
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
