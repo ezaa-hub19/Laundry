7761584a6daa8a866958f60086f56794f22ffaa1
Thoriq Hanif <thoriqmuh123.alkapro@gmail.com>
2025-02-04 13:28:44 +0700

    LaundryEzaa

--- app/src/main/java/com/ezaa/laundry/MainActivity.kt
+++ app/src/main/java/com/ezaa/laundry/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.ezaa.laundry
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.*
-import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Menampilkan tanggal saat ini di TextView
+        val tvDate = findViewById<TextView>(R.id.date_text)
+        tvDate.text = getCurrentDate()
+    }
+
+    private fun getCurrentDate(): String {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+        return currentDate.format(formatter)
     }
 }
