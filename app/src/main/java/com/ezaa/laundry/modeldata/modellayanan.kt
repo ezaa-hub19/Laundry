8409c2ff49afc804f5806d1a39d40ec1e78da9f3
Thoriq Hanif <thoriqmuh123.alkapro@gmail.com>
2025-04-30 14:53:46 +0700

    Laundry

---
+++ app/src/main/java/com/ezaa/laundry/modeldata/modellayanan.kt
@@ -0,0 +1,8 @@
+package com.ezaa.laundry.modeldata
+
+class modellayanan (
+    val idLayanan: String? = null,
+    val namaLayanan: String? = null,
+    val hargaLayanan: String? = null,
+    val cabangLayanan: String? = null
+)
